b6debd254d3954305cdc92324bf222b516b539c2
Siddharth Ambegaonkar <siddharth.ambegaonkar@bt.com>
2014-11-24 14:24:32 +0000

    Run logistic regresion to train and then predict the probability of labels on the test set

--- logRegInR.R
+++ logRegInR.R
@@ -1,10 +1,18 @@
 ## Logistic Regression in R
 
-data <- read.csv('~/code/digit_recognizer/train.csv', header=T)
+train.data <- read.csv('~/code/digit_recognizer/data/train.csv', header=T);
+test.data <- read.csv('~/code/digit_recognizer/data/test.csv', header=T);
 
-y01 <- function(value) {
-  if(value == 0) 1 else 0
+labels_vector <- unlist(train.data$label);
+
+prob.matrix <- matrix(data=NA, nrow=28000, ncol=10);
+for (i in 0:9) {
+  reg_class <- ifelse(labels_vector == i,1,0);
+  train.data$label <- reg_class;
+  
+  glm.out <- glm(label ~ ., data = train.data, family = "binomial");
+  probs <- predict(glm.out, newdata = test.data, type = "response");
+  prob.matrix[,i+1] <- probs;
 }
 
-y.zero = lapply(seq_along(data$label), y01(x, 0))
-
+head(prob.matrix)
